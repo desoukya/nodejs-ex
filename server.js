470c460fc5174ef54249d62c9a6e91c2dbdecc21
Amr Desouky <desoukya@gmail.com>
2017-07-06 13:28:28 -0400

    add version endpoint

--- server.js
+++ server.js
@@ -6,7 +6,15 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.get('/', function (req, res) {
+  console.log('hello');
   res.send('welcome to my redhat openshift demo!!');
+});
+
+app.get('/version', function (req, res) {
+  console.log('version');
+  res.json({
+    version: 'v.1.0'
+  });
 });
 
 app.listen(port, ip);
