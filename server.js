4868c735e3dc0e1de5341af7afc01e064e2936f8
Amr Desouky <desoukya@gmail.com>
2017-07-06 12:39:14 -0400

    fix code error

--- server.js
+++ server.js
@@ -2,8 +2,11 @@
 var express = require('express'),
     app     = express();
     
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
 app.get('/', function (req, res) {
-  res.send('Hello from redhat!');
+  res.send('welcome to my redhat openshift demo!');
 });
 
 app.listen(port, ip);
